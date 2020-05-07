12fb7b56961528882f2a55d1681c64d4514e1ccb
rmogusu <rmogusu123@gmail>
2020-05-07 21:50:09 +0200

    Add constructor that takes all properties as an arguments.

--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -5,7 +5,16 @@ public class Users {
     private String position;
     private String role;
     private String email;
-    private int  badgeNo;
+    private String badgeNo;
     private String department;
     private int id;
+
+    public Users(String name, String position, String role, String email, String badgeNo, String department) {
+        this.name = name;
+        this.position = position;
+        this.role = role;
+        this.badgeNo = badgeNo;
+        this.department = department;
+        this.email = email;
+    }
 }
