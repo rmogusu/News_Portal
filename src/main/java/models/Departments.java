5d72e7c0d29fef808b5f3aa2d020700bff3a6760
rmogusu <rmogusu123@gmail>
2020-05-08 00:32:38 +0200

    Add test to the department class and make sure they all pass.

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -3,18 +3,14 @@ package models;
 public class Departments {
     private String name;
     private String description;
-    private String totalNumber;
-    private String email;
-    private String address;
-    private String website;
+    private int totalNumber;
     private int id;
-    public Departments(String name, String description , String totalNumber , String email, String address , String website ) {
+
+    public Departments(String name, String description , int totalNumber ) {
         this.name = name;
         this.description  = description  ;
         this.totalNumber  =totalNumber ;
-        this.email  = email;
-        this.address  = address ;
-        this.website = website ;
+
     }
 
     public String getName() {
@@ -25,21 +21,11 @@ public class Departments {
         return description;
     }
 
-    public String getTotalNumber() {
+    public int getTotalNumber() {
         return totalNumber;
     }
 
-    public String getEmail() {
-        return email;
-    }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
 
     public int getId() {
         return id;
@@ -53,21 +39,13 @@ public class Departments {
         this.description = description;
     }
 
-    public void setTotalNumber(String totalNumber) {
+    public void setTotalNumber(int totalNumber) {
         this.totalNumber = totalNumber;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
 
-    public void setWebsite(String website) {
-        this.website = website;
-    }
+
 
     public void setId(int id) {
         this.id = id;
--- src/test/java/models/DepartmentsTest.java
+++ src/test/java/models/DepartmentsTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,41 @@ public class DepartmentsTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+    @Test
+    public void getNameReturnsCorrectName() throws Exception {
+        Departments  testDepartments = setupDepartments();
+        assertEquals("IT", testDepartments.getName());
+    }
+    @Test
+    public void getDescriptionReturnsCorrectDescription() throws Exception {
+        Departments  testDepartments = setupDepartments();
+        assertEquals("Technician support", testDepartments.getDescription());
+    }
+    @Test
+    public void getTotalNumberReturnsCorrectTotalNumber() throws Exception {
+        Departments  testDepartments = setupDepartments();
+        assertEquals(250, testDepartments.getTotalNumber());
+    }
+    @Test
+    public void setNameSetsCorrectName() throws Exception {
+        Departments  testDepartments = setupDepartments();
+        testDepartments.setName("Accounts") ;
+        assertNotEquals("IT",testDepartments.getName() );
+    }
+    @Test
+    public void setDescriptionSetsCorrectDescription() throws Exception {
+        Departments  testDepartments = setupDepartments();
+        testDepartments.setDescription("Financial operations") ;
+        assertNotEquals("Technician support",testDepartments.getDescription());
+    }
+    @Test
+    public void setTotalNumberSetsCorrectTotalNumber() throws Exception {
+        Departments  testDepartments = setupDepartments();
+        testDepartments.setTotalNumber(500) ;
+        assertNotEquals(250,testDepartments.getTotalNumber());
+    }
+    public Departments  setupDepartments (){
+        return new Departments("IT", "Technician support", 250) ;
     }
 }
