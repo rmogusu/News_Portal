6ee84b8bb5e902cb09b4304ffa331de514a9ee42
rmogusu <rmogusu123@gmail>
2020-05-07 21:44:21 +0200

    Add propeties to my object class.

---
+++ src/main/java/models/Users.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Users {
+    private String name;
+    private String position;
+    private String role;
+    private String email;
+    private int  badgeNo;
+    private String department;
+    private int id;
+}
