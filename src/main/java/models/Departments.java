8f6d774a93ebc9d448567b308efcb6c3f25ecec9
rmogusu <rmogusu123@gmail>
2020-05-08 00:14:25 +0200

    Add setter and getter methods to deprtment class.

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -16,4 +16,60 @@ public class Departments {
         this.address  = address ;
         this.website = website ;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTotalNumber() {
+        return totalNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTotalNumber(String totalNumber) {
+        this.totalNumber = totalNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
