32063b2caa7723ec3370625e7b10743569ae573e
rmogusu <rmogusu123@gmail>
2020-05-09 23:42:38 +0200

    Add logic to news class.

--- src/test/java/dao/Sql2oDepartmentsDaoTest.java
+++ src/test/java/dao/Sql2oDepartmentsDaoTest.java
@@ -2,16 +2,30 @@ package dao;
 
 import org.junit.After;
 import org.junit.Before;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import static org.junit.Assert.*;
 
 public class Sql2oDepartmentsDaoTest {
+    private Connection conn;
+    private Sql2oDepartmentsDao departmentsDao;
+    private Sql2oNewsDao newsDao;
+    private Sql2oUsersDao usersDao;
 
     @Before
     public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "rose", "wambua");
+        departmentsDao = new Sql2oDepartmentsDao(sql2o);
+        newsDao = new Sql2oNewsDao(sql2o);
+        usersDao = new Sql2oUsersDao(sql2o);
+        conn = sql2o.open();
     }
 
     @After
     public void tearDown() throws Exception {
+        conn.close();
     }
+
 }
