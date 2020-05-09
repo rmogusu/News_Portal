fee6e11b9ebd3f5bc26b938afd528d35e29be57f
rmogusu <rmogusu123@gmail>
2020-05-09 16:49:34 +0200

    Add interface to department and users class implement all the required methods.

--- src/main/java/dao/DepartmentsDao.java
+++ src/main/java/dao/DepartmentsDao.java
@@ -1,4 +1,23 @@
 package dao;
 
+import models.Departments;
+
+import java.util.List;
+
 public interface DepartmentsDao {
+    //create
+    void add (Departments  departments );
+    // void addDepartmentsToNews(Departments departments, News news)
+
+    //read
+    List<Departments> getAll();
+    Departments  findById(int id);
+    // List<News> getAllNewsForDepartments(int departmentId);
+
+    //update
+    void update(int id, String name, String description, int totalNumber);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }
--- src/main/java/dao/UsersDao.java
+++ src/main/java/dao/UsersDao.java
@@ -1,7 +1,23 @@
  package dao;
 
 
+ import models.Users;
+
+ import java.util.List;
 
  public interface UsersDao {
+  //create
+  void add(Users  users);
+
+  //read
+  List<Users> getAll();
+  List<Users> getAllUsersByDepartments(int departmentId);
+
+  //update
+  //omit for now
+
+  //delete
+  void deleteById(int id);
+  void clearAll();
 
 }
