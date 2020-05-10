a7e47fc9b61d62a36b5044d0dfc24fb573399614
rmogusu <rmogusu123@gmail>
2020-05-10 16:28:43 +0200

    Set all the required dao to my front end.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,26 @@
+import com.google.gson.Gson;
+import dao.Sql2oDepartmentsDao;
+import dao.Sql2oNewsDao;
+import dao.Sql2oUsersDao;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        Sql2oNewsDao newsDao;
+        Sql2oDepartmentsDao departmentsDao;
+        Sql2oUsersDao usersDao;
+        Connection conn;
+        Gson gson = new Gson();
+
+        String connectionString = "jdbc:h2:~/news.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "rose", "wambua");
+
+        departmentsDao = new Sql2oDepartmentsDao(sql2o);
+        newsDao = new Sql2oNewsDao(sql2o);
+        usersDao = new Sql2oUsersDao(sql2o);
+        conn = sql2o.open();
+
+    }
 }
